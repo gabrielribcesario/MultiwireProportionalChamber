881a42ca4a47edc1b196355f60fbe9d99ad2c4d4
gabrielribcesario <gabrielc.ribeiro@hotmail.com>
2025-02-01 12:25:12 -0300

    Implement explicit parallelization using OpenMP

--- src/mwpc.cpp
+++ src/mwpc.cpp
@@ -1,10 +1,12 @@
 #include <cstdlib>
 #include <string>
-#include <vector>
+#include <cmath>
+#include <omp.h>
 
 #include <TROOT.h>
 #include <TObject.h>
 #include <TTree.h>
+#include <TH1.h>
 #include <TFile.h>
 #include <TSystem.h>
 
@@ -19,6 +21,18 @@
 #include <Garfield/Random.hh>
 
 #include "CustomContainers.hpp"
+
+// Relative probabilities per 100 disintegrations given the Fe-55 X-Ray emissions table.
+
+#define KALPHA1_P 16.48  
+#define KALPHA2_P 8.40
+#define KBETA_P 3.38
+
+// X-Ray energy [eV].
+
+#define KALPHA1_E 5.89881E3
+#define KALPHA2_E 5.88772E3
+#define KBETA_E 6.49051E3
 
 using namespace Garfield;
 
@@ -35,7 +49,7 @@ int main(int argc, char* argv[]) {
                        ROOT::RCompressionSetting::EDefaults::kUseGeneralPurpose);
 
     // Sets the gas and its composition.
-    MediumMagboltz gas("Ar", 80., "CO2", 20.);
+    MediumMagboltz gas("Ar", 80.0, "CO2", 20.0);
     // Loads electron and ion transport parameters.
     gas.LoadGasFile("./GasFiles/Ar_80_CO2_20.gas");
     const std::string garfield_dir = std::getenv("GARFIELD_INSTALL");
@@ -48,42 +62,42 @@ int main(int argc, char* argv[]) {
     // Anode-Cathode gap [cm];
     const double acGap = 0.5;
     // Wire length [cm].
-    const double wireLength = 10.;
+    const double wireLength = 10.0;
 
     // Cathode wire spacing [cm].
     const double cathodeSpacing = 0.2;
     // Cathode wire diameter [cm].
     const double cathodeDiameter = 0.005;
     // Cathode wire potential [V].
-    const double cathodeV = 0.;
+    const double cathodeV = 0.0;
 
     // Anode wire spacing [cm].
     const double anodeSpacing = 0.4;
     // Anode wire diameter [cm].
     const double anodeDiameter = 0.002;
     // Anode wire potential [V].
-    const double anodeV = 2100.;
+    const double anodeV = 2100.0;
 
     MediumConductor metal;
 
     // Upper cathode wires plane, parallel to the x-axis.
-    SolidWire cathodeWireU(0., 0., acGap, 
-                           cathodeDiameter / 2., wireLength / 2., 
-                           1., 0., 0.);
+    SolidWire cathodeWireU(0.0, 0.0, acGap, 
+                           cathodeDiameter / 2.0, wireLength / 2.0, 
+                           1.0, 0.0, 0.0);
     cathodeWireU.SetBoundaryPotential(cathodeV);
     //cathodeWireU.SetLabel("CathodeWireUpper");
 
     // Lower cathode wires plane, parallel to the x-axis.
-    SolidWire cathodeWireL(0., 0., -acGap, 
-                           cathodeDiameter / 2., wireLength / 2., 
-                           1., 0., 0.);
+    SolidWire cathodeWireL(0.0, 0.0, -acGap, 
+                           cathodeDiameter / 2.0, wireLength / 2.0, 
+                           1.0, 0.0, 0.0);
     cathodeWireL.SetBoundaryPotential(cathodeV);
     //cathodeWireL.SetLabel("CathodeWireLower");
 
     // Anode wires plane, parallel to the y-axis.
-    SolidWire anodeWire(0., 0., 0., 
-                        anodeDiameter / 2., wireLength / 2., 
-                        0., 1., 0.);
+    SolidWire anodeWire(0.0, 0.0, 0.0, 
+                        anodeDiameter / 2.0, wireLength / 2.0, 
+                        0.0, 1.0, 0.0);
     anodeWire.SetBoundaryPotential(anodeV);
     //anodeWire.SetLabel("AnodeWire");
 
@@ -94,33 +108,31 @@ int main(int argc, char* argv[]) {
     mwpcGeo.AddSolid(&anodeWire, &metal);
 
     // neBEM element size.
-    const double elementSize = anodeDiameter < cathodeDiameter ? anodeDiameter / 2. : cathodeDiameter / 2.;
+    const double elementSize = anodeDiameter < cathodeDiameter ? anodeDiameter / 2.0 : cathodeDiameter / 2.0;
+
+    // Number of threads to use for parallel regions.
+    const int n_jobs = omp_get_max_threads();
 
     ComponentNeBem3d mwpc;
     mwpc.SetGeometry(&mwpcGeo);
+    mwpc.SetNumberOfThreads(n_jobs);
     mwpc.SetPeriodicityX(anodeSpacing);
     mwpc.SetPeriodicityY(cathodeSpacing);
     mwpc.SetTargetElementSize(elementSize);
     mwpc.UseLUInversion();
-    mwpc.SetNumberOfThreads(36);
     mwpc.Initialise();
 
     // Creates the interface between the transport classes and the component.
     Sensor sensor;
     sensor.AddComponent(&mwpc);
-    sensor.SetArea(-wireLength / 2., -wireLength / 2., -1.1 * (acGap + cathodeDiameter / 2.),
-                    wireLength / 2.,  wireLength / 2.,  1.1 * (acGap + cathodeDiameter / 2.));
+    sensor.SetArea(-wireLength / 2.0, -wireLength / 2.0, -1.1 * (acGap + cathodeDiameter / 2.0),
+                    wireLength / 2.0,  wireLength / 2.0,  1.1 * (acGap + cathodeDiameter / 2.0));
 
 
     // DriftLineRKF maximum step size.
     const double RKFStepSize = 0.001;
     // DriftLineRKF integration accuracy.
     const double RKFepsilon = 1.E-6;
-
-    // Creates the class responsible for tracking secondary ionizations and connects it to a sensor.
-    DriftLineRKF driftRKF(&sensor);
-    driftRKF.SetMaximumStepSize(RKFStepSize);
-    driftRKF.SetIntegrationAccuracy(RKFepsilon);
 
     std::printf("DriftLineRKF max step size [cm]: %.6f\n", RKFStepSize);
     std::printf("DriftLineRKF integration accuracy: %.6f\n", RKFepsilon);
@@ -132,15 +144,15 @@ int main(int argc, char* argv[]) {
     // Primary electron status flag.
     int status = 0;
     // Arrival time spread [ns].
-    double ats = 0.;
+    double ats = 0.0;
     // Drift line gain.
-    double gain = 0.;
+    double gain = 0.0;
     // Drift line loss.
-    double loss = 0.;
+    double loss = 0.0;
     // # of electrons at the end of the drift line.
-    double nEle = 0.;
+    double nEle = 0.0;
     // # of ions at the end of the ion tail.
-    double nIon = 0.;
+    double nIon = 0.0;
 
     TTree primaryTree("DriftLines", "Drift lines data");
     primaryTree.Branch("InitialPoint", &primaryInit);
@@ -155,36 +167,27 @@ int main(int argc, char* argv[]) {
     // Class responsible for the primary ionizations calculations.
     TrackHeed track(&sensor);
 
-    // Photon initial point, (x0, y0, z0, t0).
-    CustomContainer::Position4D photonInit;
-    // Photon initial direction, (dx, dy, dz).
-    CustomContainer::Direction3D photonDir;
-    // Number of electron-ion pairs produced by the photon.
-    int nPel = 0;
-    // Photon energy [eV].
-    double photonEnergy = 5.9E3;
+    const size_t nEvents = 10;
 
-    TTree trackTree("Photons", "Photons data");
-    trackTree.Branch("InitialPoint", &photonInit);
-    trackTree.Branch("InitialDirection", &photonDir);
-    trackTree.Branch("ElectronIonPairs", &nPel);
-    trackTree.Branch("PhotonEnergy", &photonEnergy);
+    TH1D trackHist("Fe55Spectrum", "Fe-55 Spectrum", (int) (log2(nEvents) + 1.0), 0, 250);
 
-    const size_t nEvents = 1;
+    for (int i = 0; i < nEvents ; i++) {  
+        // Photon initial point, (x0, y0, z0, t0).
+        CustomContainer::Position4D photonInit(Uniform(-0.1, 0.1), Uniform(-0.1, 0.1), 1.1 * (acGap + cathodeDiameter / 2.0), 0.0);
+        // Photon initial direction, (dx, dy, dz).
+        CustomContainer::Direction3D photonDir(Uniform(-0.1, 0.1), Uniform(-0.1, 0.1), -1.0);
+        // Photon energy [eV].
+        double photonEnergy = Uniform(0.0, KALPHA1_P + KALPHA2_P + KBETA_P);
+        photonEnergy = photonEnergy < KBETA_P ? KBETA_E : photonEnergy < KBETA_P + KALPHA1_P ? KALPHA1_E : KALPHA2_E; 
+        // Number of electron-ion pairs produced by the photon.
+        int nPel = 0;  
 
-    for (int i = 0; i < nEvents ; i++) {        
-        nPel = 0;
-        while (nPel == 0) {
-            photonInit.SetValue(Uniform(-.1, .1), Uniform(-.1, .1), 1.1 * (acGap + cathodeDiameter / 2.), 0.);
-            photonDir.SetValue(Uniform(-.1, .1), Uniform(-.1, .1), -1.);
+        track.TransportPhoton(photonInit.GetX(), photonInit.GetY(), photonInit.GetZ(), photonInit.GetT(),
+                              photonEnergy, 
+                              photonDir.GetDX(), photonDir.GetDY(), photonDir.GetDZ(),
+                              nPel);
 
-            track.TransportPhoton(photonInit.GetX(), photonInit.GetY(), photonInit.GetZ(), photonInit.GetT(),
-                                  photonEnergy, 
-                                  photonDir.GetDX(), photonDir.GetDY(), photonDir.GetDZ(),
-                                  nPel);
-        }
-        trackTree.Fill();
-        trackTree.Write(nullptr, TObject::kWriteDelete);
+        trackHist.Fill(nPel);
 
         std::printf("Event #%i\n", i + 1);
         std::printf("|  Photon energy [eV]: %#g\n", photonEnergy);
@@ -193,31 +196,45 @@ int main(int argc, char* argv[]) {
         std::printf("|  (dx, dy, dz): (%.6f, %.6f, %.6f)\n", photonDir.GetDX(), photonDir.GetDY(), photonDir.GetDZ());
 
         // Loops over the electrons produced by the photon's trajectory.
-        for (int j = 0; j < nPel; j++) {
-            double dx0 = 0., dy0 = 0., dz0 = 0.;
-            double x0 = 0., y0 = 0., z0 = 0., t0 = 0., e0 = 0.;
-            double x1 = 0., y1 = 0., z1 = 0., t1 = 0.;
+        for (const auto& cluster : track.GetClusters()) {
+            #pragma omp parallel for schedule(dynamic) num_threads(n_jobs)
+            for (const auto& electron : cluster.electrons) {
+                // Creates the class responsible for tracking secondary ionizations and connects it to a sensor.
+                DriftLineRKF driftRKF(&sensor);
+                driftRKF.SetMaximumStepSize(RKFStepSize);
+                driftRKF.SetIntegrationAccuracy(RKFepsilon);
 
-            // Retrieves the initial coordinates of the electron.
-            track.GetElectron(j, x0, y0, z0, t0, e0, dx0, dy0, dz0);
+                double x0 = electron.x, y0 = electron.y, z0 = electron.z, t0 = electron.t;
+                driftRKF.DriftElectron(x0, y0, z0, t0);
 
-            // Instantiates the tracking and avalanching of the electron.
-            driftRKF.DriftElectron(x0, y0, z0, t0);
+                int status_i = 0;
+                double x1 = 0.0, y1 = 0.0, z1 = 0.0, t1 = 0.0;
+                driftRKF.GetEndPoint(x1, y1, z1, t1, status_i);
 
-            // Retrieves the end coordinates of the electron and its status flag.
-            driftRKF.GetEndPoint(x1, y1, z1, t1, status);
+                double nEle_i = 0.0, nIon_i = 0.0;
+                driftRKF.GetAvalancheSize(nEle_i, nIon_i);
 
-            primaryInit.SetValue(x0, y0, z0, t0);
-            primaryEndP.SetValue(x1, y1, z1, t1);
-            ats = driftRKF.GetArrivalTimeSpread();
-            gain = driftRKF.GetGain();
-            loss = driftRKF.GetLoss();
-            driftRKF.GetAvalancheSize(nEle, nIon);
+                // Integration outside the critical region for better leveraging the parallel loop.
+                double ats_i = driftRKF.GetArrivalTimeSpread(), gain_i = driftRKF.GetGain(), loss_i = driftRKF.GetLoss();
 
-            primaryTree.Fill();
+                #pragma omp critical 
+                {
+                    primaryInit.SetValue(x0, y0, z0, t0);
+                    primaryEndP.SetValue(x1, y1, z1, t1);
+                    status = status_i;
+                    ats = ats_i;
+                    gain = gain_i;
+                    loss = loss_i;
+                    nEle = nEle_i;
+                    nIon = nIon_i;
+
+                    primaryTree.Fill();
+                }
+            }
         }
         primaryTree.Write(nullptr, TObject::kWriteDelete);
     }
+    trackHist.Write(nullptr, TObject::kWriteDelete);
     eventResults.Close();
 
     std::printf("Done.\n");
